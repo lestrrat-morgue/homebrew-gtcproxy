361b08256806f21474b007d7061c79c0a07ff5f3
Daisuke Maki <lestrrat+github@gmail.com>
2014-09-19 15:14:17 +0900

    commit initial version

---
+++ gtcproxy.rb
@@ -0,0 +1,25 @@
+# WARNING: Automatically generated. All changes to this file will be lost
+require 'formula'
+
+HOMEBREW_PECO_VERSION='0.0.1'
+class Peco < Formula
+  homepage 'https://github.com/lestrrat/gtcproxy'
+  url "https://github.com/lestrrat/gtcproxy/releases/download/v#{HOMEBREW_PECO_VERSION}/gtcproxy_darwin_amd64.zip"
+  sha1 "78f2e67425c263fdfadab811834e10e7b6994602"
+
+  version HOMEBREW_PECO_VERSION
+  head 'https://github.com/lestrrat/gtcproxy.git', :branch => 'master'
+
+  if build.head?
+    depends_on 'go' => :build
+    depends_on 'hg' => :build
+  end
+
+  def install
+    if build.head?
+      ENV['GOPATH'] = buildpath
+      system 'go', 'build', '.'
+    end
+    bin.install 'gtcproxy'
+  end
+end
